3fe6502473b7fa62362e68bab8e80a63928a03de
LeoGuedex <leocguedex@gmail.com>
2022-08-02 22:26:27 -0300

    fix: Insert new dependencies and properties

---
+++ src/main/java/leoguedex/com/github/API_Sales_Java/ApiSalesJavaApplication.java
@@ -0,0 +1,13 @@
+package leoguedex.com.github.API_Sales_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSalesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSalesJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leoguedex/com/github/API_Sales_Java/ApiSalesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package leoguedex.com.github.API_Sales_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSalesJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
