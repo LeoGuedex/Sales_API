5aa18b80c032f697e840b4aefa1a4ce77c609e05
LeoGuedex <leocguedex@gmail.com>
2022-08-07 01:04:44 -0300

    feat: Insert API errors handler.

---
+++ src/main/java/leoguedex/com/github/API_Sales_Java/exception/ApiErrors.java
@@ -0,0 +1,21 @@
+package leoguedex.com.github.API_Sales_Java.exception;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ApiErrors {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(List<String> errors){
+        this.errors = errors;
+    }
+
+    public ApiErrors(String errors){
+        this.errors = Arrays.asList(errors);
+    }
+
+}
---
+++ src/main/java/leoguedex/com/github/API_Sales_Java/exception/ApplicationControlerAdvice.java
@@ -0,0 +1,47 @@
+package leoguedex.com.github.API_Sales_Java.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestControllerAdvice
+public class ApplicationControlerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiErrors handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<String> collectErrors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(e -> e.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErrors(collectErrors);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ResponseStatusException.class )
+    public  ApiErrors handleResponseStatusException(ResponseStatusException ex){
+        return new ApiErrors(ex.getReason());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BusinessRulesException.class)
+    public  ApiErrors handlerBusinessRuleException(BusinessRulesException ex){
+        return new ApiErrors(ex.getMessage());
+    }
+
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(OrderNotFoundException.class)
+    public  ApiErrors handlerOrderNotFoundException(OrderNotFoundException ex){
+        return new ApiErrors(ex.getMessage());
+    }
+
+
+}
